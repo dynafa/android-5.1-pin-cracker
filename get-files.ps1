edbecace005297965f88b46416c0b86c3c75d759
dynafa <54009119+dynafa@users.noreply.github.com>
2021-06-02 20:08:33 +1200

    Add files via upload

---
+++ get-files.ps1
@@ -0,0 +1,6 @@
+adb pull /data/system/gesture.key
+adb pull /data/system/password.key
+adb pull /data/system/device_policies.xml
+adb pull /data/system/locksettings.db
+adb pull /data/system/locksettings.db-shm
+adb pull /data/system/locksettings.db-wal
